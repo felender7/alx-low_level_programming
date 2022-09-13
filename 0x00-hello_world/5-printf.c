11a831782b08319576f400c19198f3ac0891cc49
felender <felender7@mail.com>
2022-09-13 15:39:48 -0700

    updated the file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main: Entry point
+ * Description: main - Prints out a strin
  *
  * Return: Always 0 (Success)
  *
- * description: The program will print a string using printf function
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
