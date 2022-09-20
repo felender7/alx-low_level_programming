b369223531514ad30924674fee885f5a47ebacd3
felender <felender7@mail.com>
2022-09-20 14:05:04 -0700

    modified the fi;e

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,6 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -26,5 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+ 	return (0);
 }
