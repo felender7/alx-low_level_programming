ccf007ac50715f4c818add15e9c2c09e9d10f841
Felender <felender7@gmail.com>
2022-09-20 12:12:56 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (lastDigit != 0 && lastDigit < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastDigit);
 	}
 	return (0);
 }
