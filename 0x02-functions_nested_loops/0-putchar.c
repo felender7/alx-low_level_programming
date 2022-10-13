fcf7ffac8bbb9eb9e61869ab47de8766515f2ee7
Felender <felender7@gmail.com>
2022-10-13 05:21:49 +0200

    added all the files required by the task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: nothing, return void
- */
+* main - Print the word "_putchar"
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	char text[9] = "_putchar";
-        int i;
+	int i;
+	char text[] = "_putchar";
 
-	for (i = 0 ; i < 8; i++)
-	{
+	for (i = 0; i < 9; i++)
+		{
 		_putchar(text[i]);
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 	return (0);
 }
